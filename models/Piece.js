9f819cf6865d33399e1dae3e932b96e032d3caa0
Tommy Graves <tommy@infinitry.net>
2015-12-12 14:04:17 -0500

    Piece.js
    Initial Piece class

---
+++ models/Piece.js
@@ -0,0 +1,10 @@
+import Board from "Board"
+import Players from "Players"
+
+export class Piece {
+  constructor() {
+    this.location = Board.off;
+    this.owner = Players.none;
+    this.lastMove = false;
+  }
+}
