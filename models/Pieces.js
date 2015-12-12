7fd3055538d6a503e370e1bf5e67e68b6719a152
Tommy Graves <tommy@infinitry.net>
2015-12-12 14:52:02 -0500

    Pieces.js
    Initial Pieces export

---
+++ models/Pieces.js
@@ -0,0 +1,15 @@
+import King from "King";
+import Queen from "Queen";
+import Rook from "Rook";
+import Bishop from "Bishop";
+import Knight from "Knight";
+import Pawn from "Pawn";
+
+export Pieces = {
+  King: King,
+  Queen: Queen,
+  Rook: Rook,
+  Bishop: Bishop,
+  Knight: Knight,
+  Pawn: Pawn
+}
